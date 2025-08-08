1dde0d607722c2ba1650f0304684354db2722758
Youngha Park <geoffrey.youngha.park@gmail.com>
2025-08-09 03:24:53 +0900

    add code 24.2

--- template/segment_tree/RMQ.cpp
+++ template/segment_tree/RMQ.cpp
@@ -24,4 +24,18 @@ struct RMQ
         int rightMin = init(array, mid + 1, right, node * 2 + 1);
         return rangeMin[node] = min(leftMin, rightMin);
     }
+    int query(int left, int right, int node, int nodeLeft, int nodeRight)
+    {
+        if (right < nodeLeft || nodeRight < left)
+            return INT_MAX;
+        if (left <= nodeLeft && nodeRight <= right)
+            return rangeMin[node];
+        int mid = (nodeLeft + nodeRight) / 2;
+        return min(query(left, right, node * 2, nodeLeft, mid),
+                   query(left, right, node * 2 + 1, mid + 1, nodeRight));
+    }
+    int query(int left, int right)
+    {
+        return query(left, right, 1, 0, n - 1);
+    }
 };
