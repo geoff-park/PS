8a7bf4ec6b0474fc0d6389c19f6156581add4009
Youngha Park <geoffrey.youngha.park@gmail.com>
2025-08-18 00:50:30 +0900

    solve(boj): 1786

---
+++ BOJ/1786.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> getPartialMatch(const string& N) {
+  int m = N.size();
+  vector<int> pi(m, 0);
+  int begin = 1;
+  int matched = 0;
+  while (begin + matched < m) {
+    if (N[begin + matched] == N[matched]) {
+      ++matched;
+      pi[begin + matched - 1] = matched;
+    } else {
+      if (matched == 0) {
+        ++begin;
+      } else {
+        begin += matched - pi[matched - 1];
+        matched = pi[matched - 1];
+      }
+    }
+  }
+  return pi;
+}
+
+vector<int> kmpSearch(const string& H, const string& N) {
+  int n = H.size();
+  int m = N.size();
+  vector<int> ret;
+  vector<int> pi = getPartialMatch(N);
+  int begin = 0;
+  int matched = 0;
+  while (begin <= n - m) {
+    if (matched <= m && H[begin + matched] == N[matched]) {
+      ++matched;
+      if (matched == m) {
+        ret.push_back(begin);
+      }
+    } else {
+      if (matched == 0) {
+        ++begin;
+      } else {
+        begin += matched - pi[matched - 1];
+        matched = pi[matched - 1];
+      }
+    }
+  }
+  return ret;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string T;
+  string P;
+  getline(cin, T);
+  getline(cin, P);
+  vector<int> ans = kmpSearch(T, P);
+  cout << ans.size() << '\n';
+  for (int i : ans) {
+    cout << i + 1 << '\n';
+  }
+  return 0;
+}
