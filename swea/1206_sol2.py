682379ab5da7d11d2f36035e1078807fbb538546
geoff-park <geoffrey.youngha.park@gmail.com>
2025-10-04 13:24:46 +0900

    Create 1206_sol2.py

---
+++ swea/1206_sol2.py
@@ -0,0 +1,16 @@
+T = 10
+for test_case in range(1, T + 1):
+    # input
+    N = int(input())
+    lst = list(map(int, input().split()))
+
+    ans = 0
+    for i in range(2, N - 2):
+        # using max function
+        mx = max(lst[i - 2 : i] + lst[i + 1 : i + 3])
+
+        if lst[i] > mx:
+            ans += lst[i] - mx
+
+    # output
+    print(f"#{test_case} {ans}")
